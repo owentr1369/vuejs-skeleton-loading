544b53efe43d86461c029a007de672de3afdb39c
TranTam <tamvaa1306@gmail.com>
2022-07-13 09:22:30 +0700

    first commit

---
+++ src/components/skeleton-loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="skeleton-loading">
+      <h1>Skekelon Loading</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
